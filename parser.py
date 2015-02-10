fa462db5910d9fb39d31570199d7453061fafce5
Hachibu <hachibu@fastmail.fm>
2015-02-10 17:53:12 -0500

    parser.py: simplify tokenize

--- parser.py
+++ parser.py
@@ -16,54 +16,30 @@ class TokenType(enum.Enum):
     EmptySet        = 9
     Symbol          = 10
 
-Token = collections.namedtuple('Token', ['type', 'value', 'start', 'end', 'lineno'])
-
-__tokenPattern = re.compile(u'''
-      (?P<LeftBracket>{)
-    | (?P<RightBracket>})
-    | (?P<LeftParen>\()
-    | (?P<RightParen>\))
-    | (?P<Comma>,)
-    | (?P<BooleanLiteral>True|False)
-    | (?P<FloatLiteral>(-?)\d+\.\d+)
-    | (?P<IntegerLiteral>(-?)\d+)
-    | (?P<EmptySet>∅)
-    | (?P<Symbol>[^\s{}(),]+)
-''', re.VERBOSE | re.UNICODE)
-
-def __searchDict(dict, predicate):
-    return [(k, v) for k, v in dict.items() if predicate(k, v)]
+Token = collections.namedtuple('Token', 'type value start end lineno')
 
 def tokenize(string):
+    tokenPattern = re.compile(u'''
+          (?P<LeftBracket>{)
+        | (?P<RightBracket>})
+        | (?P<LeftParen>\()
+        | (?P<RightParen>\))
+        | (?P<Comma>,)
+        | (?P<BooleanLiteral>True|False)
+        | (?P<FloatLiteral>(-?)\d+\.\d+)
+        | (?P<IntegerLiteral>(-?)\d+)
+        | (?P<EmptySet>∅)
+        | (?P<Symbol>[^\s{}(),]+)
+    ''', re.VERBOSE | re.UNICODE)
     lines = string.split('\n')
     for lineno, line in enumerate(lines, start=1):
-        for match in __tokenPattern.finditer(line):
-            (key, value), = __searchDict(match.groupdict(), lambda k, v: v is not None)
+        for match in tokenPattern.finditer(line):
+            (key, value), = [(k, v) for k, v in match.groupdict().items() if v is not None]
             yield Token(TokenType[key], value, match.start(key), match.end(key), lineno)
 
 class stex(tuple): 
     def __repr__(self):
         return 'stex({})'.format(' '.join(map(str, self)))
-
-def parseCommaSepSeq(tokens, typeConstructor, delimiter):
-    result = None
-    commaSepTokens = parse(tokens)
-    if commaSepTokens == []:
-        result = typeConstructor()
-    else:
-        args = []
-        curr = []
-        for token in commaSepTokens:
-            if token in (',', delimiter):
-                if len(curr) == 1:
-                    args.append(curr.pop())
-                elif len(curr) > 1:
-                    args.append(stex(curr))
-                    curr = []
-            else:
-                curr.append(token)
-        result = typeConstructor(args)
-    return result
 
 def parse(tokens):
     result = []
@@ -98,5 +74,25 @@ def parse(tokens):
         else:
             raise Exception('Error: Unexpected token: %s' % (token,))
         token = next(tokens, None)
+    return result
+
+def parseCommaSepSeq(tokens, typeConstructor, delimiter):
+    result = None
+    commaSepTokens = parse(tokens)
+    if commaSepTokens == []:
+        result = typeConstructor()
+    else:
+        args = []
+        curr = []
+        for token in commaSepTokens:
+            if token in (',', delimiter):
+                if len(curr) == 1:
+                    args.append(curr.pop())
+                elif len(curr) > 1:
+                    args.append(stex(curr))
+                    curr = []
+            else:
+                curr.append(token)
+        result = typeConstructor(args)
     return result
 
