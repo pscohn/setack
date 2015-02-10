5909496d034a7c381594bab5189eb5469aa25073
Hachibu <hachibu@fastmail.fm>
2015-02-10 12:52:43 -0500

    update input

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import parser
 if __name__ == '__main__':
 
     syntaxTree = parser.parse(parser.tokenize(u'''
-        (1 2 add, ())
+        (1 2 add, (1, 2, add))
     '''))
 
     print(syntaxTree)
