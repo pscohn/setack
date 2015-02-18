0f48ea64573d2317c60eb2f746f7ce2d1c2bc0da
Hachibu <hachibu@fastmail.fm>
2015-02-18 17:12:25 -0500

    it works!

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import parser
 import sys
 import types
 
+trace = []
+
 def eval(value, stack, symbols):
     
     valueType = type(value)
@@ -14,41 +16,41 @@ def eval(value, stack, symbols):
         return value
 
     elif valueType in (frozenset, tuple):
-        '''
-        rs = []
+        tempStack = []
         for v in value:
-            r = eval(v, stack, symbols)
-            if r != None: 
-                rs.append(r)
-        rs = valueType(rs)
-        #stack.append(rs)
-        return rs
-        '''
+            eval(v, tempStack, symbols)
+        value = valueType(tempStack)
         stack.append(value)
         return value
 
     elif valueType == str:
-        #if value not in symbols: 
-        #    raise Exception('Undefined symbol: ' + value)
+        if value not in symbols: 
+            raise Exception('Undefined symbol: ' + value)
         symbolValue = symbols[value]
         symbolType  = type(symbolValue)
         if symbolType is types.FunctionType:
             return symbolValue(stack)
         else:
+            stack.append(symbolValue)
             return symbolValue
 
     elif valueType == parser.SetExp:
-        rs = []
+        returnValues = []
         for v in value:
             r = eval(v, stack, symbols)
-            #print(v, '=>', r)
             if r != None: 
-                rs.append(r)
-        if len(rs):
-            return rs.pop()
+                returnValues.append(r)
+            trace.append('{} => {}'.format(v, r))
+        if len(returnValues):
+            return returnValues.pop()
+        else:
+            return None
     else:
         raise Exception('Unexpected value: ' + value)
     
+# Built-ins
+# ------------------------------------------------------------------------------
+
 def println(stack):
     if stack: print(stack[-1])
 
@@ -62,15 +64,29 @@ def union(stack):
     stack.append(r)
     return r
 
+def add(stack):
+    a = stack.pop()
+    b = stack.pop()
+    r = a + b
+    stack.append(r)
+    return r
+
 if __name__ == '__main__':
 
-    input = u'''{1, 2} {3, 4} union print dump'''
-
+    input = u'''
+        {x 2 +} {4} union println
+    '''
     syntaxTree = parser.Parser().parse(input)
 
     stack   = []
-    symbols = {'union': union, 'print': println, 'dump': dump}
+    symbols = {'union'  : union, 
+               'println': println, 
+               'dump'   : dump, 
+               '+'      : add, 
+               'x'      : 1}
 
-    print('input: "{}"'.format(input))
     eval(syntaxTree, stack, symbols)
 
+    print()
+    for step, inst in enumerate(trace, start=1):
+        print('{}: {}'.format(step, inst))
