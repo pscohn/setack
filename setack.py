204647d737880101a26e94eec1c9c05ea39a5815
Hachibu <hachibu@fastmail.fm>
2015-02-19 16:34:13 -0500

    roughly copy pasta some autocomplete for repl

--- setack.py
+++ setack.py
@@ -7,6 +7,48 @@ import vm
 
 from colorprint import *
 
+class MyCompleter(object):  # Custom completer
+
+    def __init__(self, options):
+        self.options = sorted(options)
+
+    def complete(self, text, state):
+        if state == 0:  # on first trigger, build possible matches
+            if not text:
+                self.matches = self.options[:]
+            else:
+                self.matches = [s for s in self.options
+                                if s and s.startswith(text)]
+
+        # return match indexed by state
+        try:
+            return self.matches[state]
+        except IndexError:
+            return None
+
+    def display_matches(self, substitution, matches, longest_match_length):
+        line_buffer = readline.get_line_buffer()
+        columns = environ.get("COLUMNS", 80)
+
+        print()
+
+        tpl = "{:<" + str(int(max(map(len, matches)) * 1.2)) + "}"
+
+        buffer = ""
+        for match in matches:
+            match = tpl.format(match[len(substitution):])
+            if len(buffer + match) > columns:
+                print(buffer)
+                buffer = ""
+            buffer += match
+
+        if buffer:
+            print(buffer)
+
+        print("> ", end="")
+        print(line_buffer, end="")
+        sys.stdout.flush()
+
 history = '{}/{}'.format(os.path.expanduser('~'), '.setack_history')
 vm = vm.VM()
 
@@ -14,6 +56,17 @@ if not os.path.exists(history):
     open(history, 'w')
 
 readline.read_history_file(history)
+
+keywords = '''
+    depth in show-stack clear symmetric-difference not-in subset ! union
+    . power-set difference cartesian-product drop intersection show-symbols
+'''.split()
+completer = MyCompleter(keywords)
+
+readline.set_completer_delims(' \t\n;')
+readline.set_completer(completer.complete)
+readline.parse_and_bind('tab: complete')
+readline.set_completion_display_matches_hook(completer.display_matches)
 
 try:
 
